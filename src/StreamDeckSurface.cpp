32f2a1b29faa5bc9d79b1bc8ea9c4469d0ebb9d5
Versatophon <versatophon@gmail.com>
2025-06-27 22:12:15 +0200

    Bring consistency for image loading (PNG and BMP) in UI

--- src/StreamDeckSurface.cpp
+++ src/StreamDeckSurface.cpp
@@ -10,6 +10,7 @@
 
 #include <SDL3/SDL.h>
 #include <turbojpeg.h>
+#include <FreeImage.h>
 
 #include "Providers.h"
 
@@ -27,13 +28,63 @@ struct StreamDeckSurfaceID
     std::string Filepath;
     SdlResourcesProvider* SdlProvider;
     TurboJpegResourcesProvider* TjProvider;
-    ImageType RawType = ImageType::UKN;
-    std::vector<uint8_t> RawFileData;
+    //ImageType RawType = ImageType::UKN;
+    //std::vector<uint8_t> RawFileData;
 
     bool IsValid = false;
 
     SDL_Surface* Surface;
     SDL_Texture* Texture;
+
+    void GenerateSurfaceFromJpg(std::vector<uint8_t>& pRawFileData)
+    {
+        int32_t lWidth = -1;
+        int32_t lHeight = -1;
+
+        if ( 0 != tjDecompressHeader(TjProvider->GetDecompressor(), pRawFileData.data(), pRawFileData.size(), &lWidth, &lHeight) )
+        {
+            std::cout << "jpeg header extraction failed" << std::endl;
+        }
+        else
+        {
+            Surface = SDL_CreateSurface(lWidth, lHeight, SDL_PIXELFORMAT_BGRA32);
+            //lImage.Content.resize(lImage.Width * lImage.Height * 4);
+
+            if( 0 != tjDecompress2(TjProvider->GetDecompressor(), pRawFileData.data(), pRawFileData.size(), 
+                                   (uint8_t*)Surface->pixels, Surface->w, Surface->pitch, Surface->h,
+                                   TJPF_BGRA, TJFLAG_ACCURATEDCT))
+            {
+                std::cout << "jpeg decompression failed" << std::endl;
+            }
+
+            //Texture = SDL_CreateTextureFromSurface(SdlProvider->GetSdlRenderer(), Surface);
+
+            IsValid = true;
+        }
+    }
+
+    void GenerateSurfaceFromBmp(std::vector<uint8_t>& pRawFileData)
+    {
+        FIMEMORY* lFiMemory = FreeImage_OpenMemory(pRawFileData.data(), pRawFileData.size());
+
+        FIBITMAP* lBitmap = FreeImage_LoadFromMemory(FIF_BMP, lFiMemory);
+        FIBITMAP* lBitmapBGRA = FreeImage_ConvertTo32Bits(lBitmap);
+        FreeImage_FlipHorizontal(lBitmapBGRA);
+        FreeImage_FlipVertical(lBitmapBGRA);
+
+        Surface = SDL_CreateSurface(FreeImage_GetWidth(lBitmapBGRA), FreeImage_GetHeight(lBitmapBGRA), SDL_PIXELFORMAT_BGRA32);
+        
+        memcpy(Surface->pixels, FreeImage_GetBits(lBitmapBGRA), Surface->h * Surface->pitch);
+
+        //Texture = SDL_CreateTextureFromSurface(SdlProvider->GetSdlRenderer(), Surface);
+
+        //Free memory
+        FreeImage_Unload(lBitmapBGRA);
+        FreeImage_Unload(lBitmap);
+        FreeImage_CloseMemory(lFiMemory);
+
+        IsValid = true;
+    }
 };
 
 ImageType GetImageType(std::vector<uint8_t>& pFileData)
@@ -100,45 +151,37 @@ StreamDeckSurface::StreamDeckSurface(const char* pFilepath, SdlResourcesProvider
     size_t lFileSize = lFile.tellg();
     lFile.seekg(0, std::ios_base::beg);
 
+    std::vector<uint8_t> lRawFileData;
+    ImageType lImageType = ImageType::UKN;
+
     //TODO: check here if file is jpeg and valid
-    mID->RawFileData.resize(lFileSize);
-    lFile.read((char*)mID->RawFileData.data(), lFileSize);
+    lRawFileData.resize(lFileSize);
+    lFile.read((char*)lRawFileData.data(), lFileSize);
 
     try 
     {
-        mID->RawType = GetImageType(mID->RawFileData);
+        lImageType = GetImageType(lRawFileData);
     }
     catch (const std::domain_error& pException)
     {
         std::cout << "Error loading " << pFilepath << ": " << pException.what() << std::endl;
     }
 
-    if( mID->RawType == ImageType::JPG )
+    switch (lImageType)
     {
-        int32_t lWidth = -1;
-        int32_t lHeight = -1;
+        case ImageType::JPG:
+            mID->GenerateSurfaceFromJpg(lRawFileData);
+            break;
 
-        if ( 0 != tjDecompressHeader(mID->TjProvider->GetDecompressor(), mID->RawFileData.data(), mID->RawFileData.size(), &lWidth, &lHeight) )
-        {
-            std::cout << "jpeg header extraction failed" << std::endl;
-        }
-        else
-        {
-            mID->Surface = SDL_CreateSurface(lWidth, lHeight, SDL_PIXELFORMAT_RGBA32);
-            //lImage.Content.resize(lImage.Width * lImage.Height * 4);
-
-            if( 0 != tjDecompress2(mID->TjProvider->GetDecompressor(), mID->RawFileData.data(), mID->RawFileData.size(), (uint8_t*)mID->Surface->pixels, 
-                                    mID->Surface->w, mID->Surface->pitch, mID->Surface->h, TJPF_RGBA, TJFLAG_ACCURATEDCT))
-            {
-                std::cout << "jpeg decompression failed" << std::endl;
-            }
-        }
-
-        mID->Texture = SDL_CreateTextureFromSurface(mID->SdlProvider->GetSdlRenderer(), mID->Surface);
-
-        mID->IsValid = true;
+        case ImageType::BMP:
+            mID->GenerateSurfaceFromBmp(lRawFileData);
+            break;
     }
-    
+
+    if(mID->IsValid )
+    {
+        mID->Texture = SDL_CreateTextureFromSurface(mID->SdlProvider->GetSdlRenderer(), mID->Surface);
+    }
 }
 
 StreamDeckSurface::~StreamDeckSurface()
